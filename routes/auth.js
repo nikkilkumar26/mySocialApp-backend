a791f636e3406c27735a64f11b33d3feb51f01c6
nikkilkumar26 <71882426+nikkilkumar26@users.noreply.github.com>
2020-10-29 16:38:36 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -117,7 +117,7 @@ router.post('/resetpassword',(req,res)=>{
                     to: user.email,
                     subject: 'Password Reset',
                     html: `<p> You are requested for Password reset </p>
-                            <h5>Click in this <a href="https://nikkil-global-media-site.netlify.app/${token}">link</a> to reset password</h5>`
+                            <h5>Click in this <a href="https://nikkil-global-media-site.netlify.app/reset/${token}">link</a> to reset password</h5>`
                   })
             }).then( res.json({message:"Check you Email Inbox if not check Spam"}))
            
